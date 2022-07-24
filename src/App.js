5550b47f17a8a7d79a990130c126f8c15a61074b
michaelphillips4 <31538839+michaelphillips4@users.noreply.github.com>
2022-07-24 19:15:38 +0100

    SetUp and Create DataSet

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import LottoPredict from './Components/Lotto/Lotto-predict';
+
+function App() {
+  return (
+    <div >
+     <h1 className="center">ML Tensor Flow Experiment</h1>
+
+
+    <LottoPredict />
+
+    </div>
+  );
+}
+
+export default App;
